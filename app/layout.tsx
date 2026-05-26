4b0a6a6132755c0e8f632bb944a77aa2cb1128c3
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2026-05-26 11:23:39 -0300

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,10 @@
+import "@/app/ui/global.css";
+import { geist } from "@/app/ui/fonts";
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={`${geist.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Geist, Lusitana } from "next/font/google";
+
+export const geist = Geist({ subsets: ["latin"] });
+
+export const lusitana = Lusitana({
+  weight: ["400", "700"],
+  subsets: ["latin"]
+});
