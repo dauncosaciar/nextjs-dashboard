11beefa881d9b562f2ecc54b82e40754a0860dea
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2026-07-02 14:00:49 -0300

    add server side validation for edit-form

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -70,15 +70,27 @@ export async function createInvoice(prevState: State, formData: FormData) {
   redirect("/dashboard/invoices");
 }
 
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
+  // Validate form fields using Zod
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get("customerId"),
     amount: formData.get("amount"),
     status: formData.get("status")
   });
 
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing Fields. Failed to Update Invoice."
+    };
+  }
+
+  // Prepare data for update into the database
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
 
+  // Update data into the database
   try {
     await sql`
       UPDATE invoices
@@ -86,12 +98,13 @@ export async function updateInvoice(id: string, formData: FormData) {
       WHERE id = ${id}
     `;
   } catch (error) {
-    console.error(error);
+    // If a database error occurs, return a more specific error.
     return {
       message: "Database Error: Failed to Update Invoice."
     };
   }
 
+  // Revalidate the cache for the invoices page and redirect the user.
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
 }
